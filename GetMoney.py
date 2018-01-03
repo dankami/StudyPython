6f9e718dc5246ce9420b0c8b633b1af2d1f8ccc2
Xiankai Zhang <413619985@qq.com>
2018-01-03 15:30:28 +0800

    提交数据处理

--- GetMoney.py
+++ GetMoney.py
@@ -1,7 +1,6 @@
 #coding=utf-8
 
 # 取款机
-from pyecharts import Bar
 
 if __name__ == '__main__':
     import tushare as ts
@@ -9,8 +8,31 @@ if __name__ == '__main__':
     # myData = ts.get_hist_data('600848', start='2015-01-05', end='2015-01-09') # 设定时间
     # myData = ts.get_hist_data(code = 'hs300')
     # print(myData)
+    df = ts.get_tick_data('601398', date='2018-01-02')
+    # print(df.head(10))
 
-    bar = Bar("我的第一个图表", "这里是副标题")
-    bar.add("服装", ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90])
-    bar.show_config()
-    bar.render()
+    bAmount = 0
+    sAmount = 0
+    xAmount = 0
+    allAmount = 0
+    dfLen = len(df)
+    for i in range(dfLen):
+        moneyData = df.iloc[i]
+        allAmount = allAmount + moneyData.amount
+        if moneyData.type == "买盘" :
+            bAmount = bAmount + moneyData.amount
+        elif moneyData.type == "卖盘":
+            sAmount = sAmount + moneyData.amount
+        else:
+            xAmount = xAmount + moneyData.amount
+
+    print(allAmount)
+    print(sAmount + bAmount + xAmount)
+    print(sAmount)
+    print(bAmount)
+    print(xAmount)
+
+
+
+
+
