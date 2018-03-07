5573289119547625ff0b34104fa7e9d1148e2954
kami-jo <763757511@qq.com>
2018-03-07 23:06:18 +0800

    完成 下载脚本

--- TuShare/Dm01_Hello.py
+++ TuShare/Dm01_Hello.py
@@ -2,9 +2,35 @@
 
 import tushare as ts
 
+def saveHistData(_code):
+    print '下载：', _code
+    data = ts.get_hist_data(_code)  # 一次性获取全部日k线数据
+    if not data is None:
+        print '保存：', _code
+        data.to_csv('hist/%s.csv'%(_code))
+        print '保存完成'
+
+def saveQHistData(_code):
+    print '下载：', _code
+    data = ts.get_hist_data(_code)  # 一次性获取全部日k线数据
+    if not data is None:
+        print '保存：', _code
+        data.to_csv('qhist/%s.csv'%(_code))
+        print '保存完成'
+
 if __name__ == '__main__':
-    data = ts.get_hist_data('600104')  # 一次性获取全部日k线数据
-    print data
-    data.to_csv('600104.csv')
-    data.to_json('600104.json')
-    data.to_excel('600104.xlsx')
+    for i in range(1000):
+        saveHistData('000%03d' % (i))
+        saveHistData('001%03d' % (i))
+        saveHistData('002%03d' % (i))
+        saveHistData('600%03d' % (i))
+        saveHistData('601%03d' % (i))
+        saveHistData('603%03d' % (i))
+
+        # saveQHistData('000%03d' % (i))
+        # saveQHistData('001%03d' % (i))
+        # saveQHistData('002%03d' % (i))
+        # saveQHistData('600%03d' % (i))
+        # saveQHistData('601%03d' % (i))
+        # saveQHistData('603%03d' % (i))
+
