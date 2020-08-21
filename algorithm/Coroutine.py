6ea3e50e431e608f954fd5a86de1c919f53f9c0a
ChenXIongZhou <chenxiongzhou@myths.com>
2020-08-21 12:10:22 +0800

    新增 python协程demo

--- algorithm/Coroutine.py
+++ algorithm/Coroutine.py
@@ -1,21 +1,37 @@
-    
-def foo():
-    print("starting...")
-    while True:
-        res = yield 4
-        print("res:",res)
-g = foo()
-print(next(g))
-print("*"*20)
-print(next(g))
+# 协程demo
 
-def consume():
-    while True:
-        number = yield
-        print("开始消费", number)
+# 生产消费者模式
+# def foo():
+#     print("starting...")
+#     while True:
+#         res = yield 4
+#         print("res:",res)
+# g = foo()
+# print(next(g))
+# print("*"*20)
+# print(next(g))
+
+# def consume():
+#     while True:
+#         number = yield
+#         print("开始消费", number)
         
-consumer = consume()
-next(consumer)
-for num in range(0, 100):
-    print("开始生产", num)
-    consumer.send(num)
+# consumer = consume()
+# next(consumer)
+# for num in range(0, 100):
+#     print("开始生产", num)
+#     consumer.send(num)
+
+
+import time
+
+def countNum():
+    for num in range(0, 10):
+        print(num)
+        time.sleep(1)
+    yield
+
+conCountNum = countNum()
+next(conCountNum)    
+
+print("hello")    
